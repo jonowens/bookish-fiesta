053b5b7f85932ba2f63e6ff6ef943e3ede9b8140
jonowens <jonathan.owens@gmail.com>
2021-03-26 22:50:39 -0500

    Created bbands_inside_kchannels function.

--- libs/signals.py
+++ libs/signals.py
@@ -26,3 +26,20 @@ def macd(dataframe_name, signal_lag = 9):
     return dataframe_name
 
 # Bollingerbands inside Keltner Channels signals
+def bbands_inside_kchannels(dataframe_name):
+    """Creates signals for long position
+    Args:
+        dataframe_name (df): Dataframe containing indicator data for Bollinger Bands and Keltner Channels
+    Returns:
+        A dataframe of:
+            original data passed to function,
+            squeeze (flt): Signals (1.0 = True, 0.0 = False),
+    """
+
+    # Create signal for bollinger band is inside keltner channel
+    selection = dataframe_name.loc[((dataframe_name['bb_upper'] < dataframe_name['kc_upper']) & (dataframe_name['bb_lower'] >= dataframe_name['kc_lower'])), :].index
+    dataframe_name['squeeze'] = 0.0
+    dataframe_name['squeeze'][selection] = 1
+
+    # Return dataframe with features and target
+    return dataframe_name
